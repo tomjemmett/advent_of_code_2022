a834e445c49931ae8ad97d70dcf5191980c22045
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-01 10:00:42 +0000

    refactor day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -6,14 +6,7 @@ import Common
 import Data.List.Split (splitOn)
 
 day01 :: AOCSolution
-day01 input = show <$> [p1, p2]
+day01 input = show . sum <$> r
   where
-    i = sortDesc . map sum . map (map (read::String->Int)) . map lines . splitOn "\n\n" $ input
-    p1 = head i
-    p2 = sum $ take 3 i
-
--- check that each item in the list is less than the nth next item
--- for part 1, n = 1 (the next item)
--- for part 2, n = 3: sum (a, b, c) < sum (b, c, d) iff a < d
-countIncrements :: Int -> [Int] -> String
-countIncrements n x = show $ countTrue id $ zipWith (<) x (drop n x)
+    f = sortDesc . map (sum . linesRead) . splitOn "\n\n"
+    r = take <$> [1, 3] <*> pure (f input)
