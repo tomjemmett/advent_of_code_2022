05e84fc09a051479c17528e0f24a07e01970e9bf
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-03 10:32:43 +0000

    adds day 3 solution

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,8 +1,37 @@
 module Day03 (
-  day03
+  day03,
+  part1,
+  part2
 ) where
 
 import Common
+import Data.Char (ord)
+import Data.List (nub, sort)
 
 day03 :: AOCSolution
-day03 input = undefined
+day03 input = show . sum <$> ([part1, part2] <*> pure (lines input))
+
+part1 :: [String] -> [Int]
+part2 :: [String] -> [Int]
+part1 = map (getPriority . head . findCommon 2 . splitLine)
+part2 = map getPriority . findCommon 3 . map (nub . sort)
+
+splitLine :: String -> [String]
+splitLine line = (nub . sort) <$> ([take, drop] <*> pure n <*> pure line)
+  where
+    n = (length line) `div` 2
+
+getPriority :: String -> Int
+getPriority (c:_) = ord c - if c < 'a' then 38 else 96
+
+findCommon :: Int -> [String] -> [String]
+findCommon _ [] = []
+findCommon n xs = (foldl1 f $ take n xs):(findCommon n $ drop n xs)
+  where
+    f:: String -> String -> String
+    f _ [] = []
+    f [] _ = []
+    f (x:xs) (y:ys)
+      | x == y = x:f xs ys
+      | x >  y    = f (x:xs) ys
+      | otherwise = f xs (y:ys)
--- test/Day03Spec.hs
+++ test/Day03Spec.hs
@@ -2,15 +2,20 @@ module Day03Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
+            \jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+            \PmmdzqPrVvPwwTWBwg\n\
+            \wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+            \ttgJtRGJQctTZtZT\n\
+            \CrZsJsPPZsGzwwsLwLmpwMDw"
 
 spec :: Spec
 spec = describe "Day 3" $ do
   return ()
-  -- it "Sample" $ do
-  --     day03 testInput `shouldBe` ["", ""]
+  it "Sample" $ do
+      day03 testInput `shouldBe` ["157", "70"]
       
-  -- it "Actual" $ do
-  --   withFile "inputs/day03.txt" ReadMode (\h -> do
-  --     actualInput <- hGetContents h
-  --     day03 actualInput `shouldBe` ["",""])
+  it "Actual" $ do
+    withFile "inputs/day03.txt" ReadMode (\h -> do
+      actualInput <- hGetContents h
+      day03 actualInput `shouldBe` ["7746", "2604"])
