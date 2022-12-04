038f0a9611fafd67d65b88702459e57a78f3bfd9
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-04 10:11:25 +0000

    adds day 4 solution

--- src/Day04.hs
+++ src/Day04.hs
@@ -3,6 +3,26 @@ module Day04 (
 ) where
 
 import Common
+import Data.List (sort, group)
+import Data.List.Split (splitOn)
+
+data Overlaps = None | Full | Partial deriving (Show, Eq, Ord)
 
 day04 :: AOCSolution
-day04 input = undefined
+day04 input = show <$> scanl1 (+) i
+  where
+    i = tail $ map length $ group $ sort $ overlaps <$> parseInput input
+
+parseInput :: String -> [[[Int]]]
+parseInput = map splitLine . lines
+  where
+    splitRange = map read . splitOn "-"
+    splitLine = map splitRange . splitOn ","
+
+overlaps :: [[Int]] -> Overlaps
+overlaps [[a,b],[c,d]]
+  | a <= c && b >= d = Full
+  | c <= a && d >= b = Full
+  | a <= d && b >= c = Partial
+  | d <= a && c >= b = Partial
+  | otherwise        = None
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -2,15 +2,20 @@ module Day04Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "2-4,6-8\n\
+            \2-3,4-5\n\
+            \5-7,7-9\n\
+            \2-8,3-7\n\
+            \6-6,4-6\n\
+            \2-6,4-8"
 
 spec :: Spec
 spec = describe "Day 4" $ do
   return ()
-  -- it "Sample" $ do
-  --     day04 testInput `shouldBe` ["", ""]
+  it "Sample" $ do
+      day04 testInput `shouldBe` ["2", "4"]
       
-  -- it "Actual" $ do
-  --   withFile "inputs/day04.txt" ReadMode (\h -> do
-  --     actualInput <- hGetContents h
-  --     day04 actualInput `shouldBe` ["",""])
+  it "Actual" $ do
+    withFile "inputs/day04.txt" ReadMode (\h -> do
+      actualInput <- hGetContents h
+      day04 actualInput `shouldBe` ["602","891"])
