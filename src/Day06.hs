fdfbea35cf5b46e1e3117a55714633d700f0cf33
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-06 08:54:45 +0000

    adds day 6 solution

--- src/Day06.hs
+++ src/Day06.hs
@@ -3,6 +3,14 @@ module Day06 (
 ) where
 
 import Common
+import Data.List (nub)
 
 day06 :: AOCSolution
-day06 input = undefined
+day06 input = go . f <$> [4, 14] <*> pure input
+  where
+    f x = (x, x)
+
+go :: (Int, Int) -> String -> String
+go (i, n) x
+  | (length $ nub $ take n x) == n = show i
+  | otherwise = go (succ i, n) $ drop 1 x
--- test/Day06Spec.hs
+++ test/Day06Spec.hs
@@ -2,15 +2,14 @@ module Day06Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
 
 spec :: Spec
 spec = describe "Day 6" $ do
-  return ()
-  -- it "Sample" $ do
-  --    day06 testInput `shouldBe` ["", ""]
+  it "Sample" $ do
+     day06 testInput `shouldBe` ["7", "19"]
       
-  -- it "Actual" $ do
-  --  withFile "inputs/day06.txt" ReadMode (\h -> do
-  --    actualInput <- hGetContents h
-  --    day06 actualInput `shouldBe` ["",""])
+  it "Actual" $ do
+   withFile "inputs/day06.txt" ReadMode (\h -> do
+     actualInput <- hGetContents h
+     day06 actualInput `shouldBe` ["1707","3697"])
