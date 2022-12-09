9385ea28a851814b1ebb68af846540f2747e9ce5
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-09 12:47:33 +0000

    adds day 9 solution

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,8 +1,56 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# LANGUAGE TupleSections #-}
 module Day09 (
   day09
 ) where
 
 import Common
+import qualified Data.HashSet as H
+import Control.Monad.State
+
+type Visited = H.HashSet Point2d
 
 day09 :: AOCSolution
-day09 input = undefined
+day09 input = go <$> [2, 10] <*> parseInput input
+
+parseInput :: Applicative f => String -> f [(Char, Int)]
+parseInput = pure . foldr f [] . lines
+  where
+    f :: [Char] -> [(Char, Int)] -> [(Char, Int)]
+    f (m:' ':(read -> s)) p = (m, s):p
+
+go :: Int -> [(Char, Int)] -> String
+go n = show . H.size . f (replicate n (0, 0), mempty)
+  where
+    f :: ([Point2d], Visited) -> [(Char, Int)] -> Visited
+    f (_, v) [] = v
+    f (p, v) (m:ms) = f (runState (move m p) v) ms
+
+move :: (Char, Int) -> [Point2d] -> State Visited [Point2d]
+move (_, 0) p = state (p,)
+move (m, n) (hp:tp) = do
+  v <- get
+  let
+    hp' = moveH hp m
+    p' = scanl moveT hp' tp
+  put $ H.insert (last p') v
+  move (m, pred n) p'
+
+moveH :: Point2d -> Char -> Point2d
+moveH (hx, hy) = \case
+  'R' -> (hx, succ hy)
+  'L' -> (hx, pred hy)
+  'U' -> (succ hx, hy)
+  'D' -> (pred hx, hy)
+
+moveT :: Point2d -> Point2d -> Point2d
+moveT (hx, hy) (tx, ty)
+  | dx <= 1 && dy <= 1   = (tx, ty)
+  | dx == 2 && dy == 0   = (f hx tx, ty)
+  | dx == 0 && dy == 2   = (tx, f hy ty)
+  | otherwise            = (f hx tx, f hy ty)
+  where
+    f a b = if a < b then pred b else succ b
+    dx = abs (hx - tx)
+    dy = abs (hy - ty)
--- test/Day09Spec.hs
+++ test/Day09Spec.hs
@@ -2,15 +2,31 @@ module Day09Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput1 = "R 4\n\
+             \U 4\n\
+             \L 3\n\
+             \D 1\n\
+             \R 4\n\
+             \D 1\n\
+             \L 5\n\
+             \R 2"
+
+testInput2 = "R 5\n\
+             \U 8\n\
+             \L 8\n\
+             \D 3\n\
+             \R 17\n\
+             \D 10\n\
+             \L 25\n\
+             \U 20"
 
 spec :: Spec
 spec = describe "Day 9" $ do
-  return ()
-  -- it "Sample" $ do
-  --     day09 testInput `shouldBe` ["", ""]
+  it "Sample" $ do
+      day09 testInput1 `shouldBe` ["13", "1"]
+      day09 testInput2 `shouldBe` ["88", "36"]
       
-  -- it "Actual" $ do
-  --   withFile "inputs/day09.txt" ReadMode (\h -> do
-  --     actualInput <- hGetContents h
-  --     day09 actualInput `shouldBe` ["",""])
+  it "Actual" $ do
+    withFile "inputs/day09.txt" ReadMode (\h -> do
+      actualInput <- hGetContents h
+      day09 actualInput `shouldBe` ["6494","2691"])
