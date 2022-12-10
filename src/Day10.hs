d3b727e145cca1304908c4ddf1728f9e57675704
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-10 09:56:32 +0000

    adds day 10 solution

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,8 +1,36 @@
+{-# LANGUAGE ViewPatterns #-}
 module Day10 (
   day10
 ) where
 
+
 import Common
+import Data.List.Split (chunksOf)
 
 day10 :: AOCSolution
-day10 input = undefined
+day10 input = [part1, part2] <*> pure (parseInput input)
+
+part1 :: [Int] -> String
+part1 = show . sum . f 20 . drop 19
+  where
+    f _ [] = []
+    f i (x:xs) = (x * i):g i xs
+    g i = f (i + 40) . drop 39
+
+part2 :: [Int] -> String
+part2 = unlines . chunksOf 40 . go . zip [0..]
+  where
+    go [] = []
+    go ((x, y):xs) = p:go xs
+      where
+        p = if abs ((x `mod` 40) - y) <= 1
+          then '#'
+          else '.'
+
+parseInput :: String -> [Int]
+parseInput = (1:) . parseLine 1 . lines
+  where
+    parseLine :: Int -> [String] -> [Int]
+    parseLine _ [] = []
+    parseLine regx ("noop":xs) = regx:parseLine regx xs
+    parseLine regx (((regx+) . read . drop 5 -> n):xs) = regx:n:parseLine n xs
--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -2,15 +2,175 @@ module Day10Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "addx 15\n\
+            \addx -11\n\
+            \addx 6\n\
+            \addx -3\n\
+            \addx 5\n\
+            \addx -1\n\
+            \addx -8\n\
+            \addx 13\n\
+            \addx 4\n\
+            \noop\n\
+            \addx -1\n\
+            \addx 5\n\
+            \addx -1\n\
+            \addx 5\n\
+            \addx -1\n\
+            \addx 5\n\
+            \addx -1\n\
+            \addx 5\n\
+            \addx -1\n\
+            \addx -35\n\
+            \addx 1\n\
+            \addx 24\n\
+            \addx -19\n\
+            \addx 1\n\
+            \addx 16\n\
+            \addx -11\n\
+            \noop\n\
+            \noop\n\
+            \addx 21\n\
+            \addx -15\n\
+            \noop\n\
+            \noop\n\
+            \addx -3\n\
+            \addx 9\n\
+            \addx 1\n\
+            \addx -3\n\
+            \addx 8\n\
+            \addx 1\n\
+            \addx 5\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \addx -36\n\
+            \noop\n\
+            \addx 1\n\
+            \addx 7\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \addx 2\n\
+            \addx 6\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \addx 1\n\
+            \noop\n\
+            \noop\n\
+            \addx 7\n\
+            \addx 1\n\
+            \noop\n\
+            \addx -13\n\
+            \addx 13\n\
+            \addx 7\n\
+            \noop\n\
+            \addx 1\n\
+            \addx -33\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \addx 2\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \addx 8\n\
+            \noop\n\
+            \addx -1\n\
+            \addx 2\n\
+            \addx 1\n\
+            \noop\n\
+            \addx 17\n\
+            \addx -9\n\
+            \addx 1\n\
+            \addx 1\n\
+            \addx -3\n\
+            \addx 11\n\
+            \noop\n\
+            \noop\n\
+            \addx 1\n\
+            \noop\n\
+            \addx 1\n\
+            \noop\n\
+            \noop\n\
+            \addx -13\n\
+            \addx -19\n\
+            \addx 1\n\
+            \addx 3\n\
+            \addx 26\n\
+            \addx -30\n\
+            \addx 12\n\
+            \addx -1\n\
+            \addx 3\n\
+            \addx 1\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \addx -9\n\
+            \addx 18\n\
+            \addx 1\n\
+            \addx 2\n\
+            \noop\n\
+            \noop\n\
+            \addx 9\n\
+            \noop\n\
+            \noop\n\
+            \noop\n\
+            \addx -1\n\
+            \addx 2\n\
+            \addx -37\n\
+            \addx 1\n\
+            \addx 3\n\
+            \noop\n\
+            \addx 15\n\
+            \addx -21\n\
+            \addx 22\n\
+            \addx -6\n\
+            \addx 1\n\
+            \noop\n\
+            \addx 2\n\
+            \addx 1\n\
+            \noop\n\
+            \addx -10\n\
+            \noop\n\
+            \noop\n\
+            \addx 20\n\
+            \addx 1\n\
+            \addx 2\n\
+            \addx 2\n\
+            \addx -6\n\
+            \addx -11\n\
+            \noop\n\
+            \noop\n\
+            \noop"
+
+sp2 = "##..##..##..##..##..##..##..##..##..##..\n\
+      \###...###...###...###...###...###...###.\n\
+      \####....####....####....####....####....\n\
+      \#####.....#####.....#####.....#####.....\n\
+      \######......######......######......####\n\
+      \#######.......#######.......#######.....\n\
+      \.\n"
+
+ap2 = "####...##.#..#.###..#..#.#....###..####.\n\
+      \#.......#.#..#.#..#.#..#.#....#..#....#.\n\
+      \###.....#.#..#.###..#..#.#....#..#...#..\n\
+      \#.......#.#..#.#..#.#..#.#....###...#...\n\
+      \#....#..#.#..#.#..#.#..#.#....#.#..#....\n\
+      \#.....##...##..###...##..####.#..#.####.\n\
+      \.\n"
 
 spec :: Spec
 spec = describe "Day 10" $ do
-  return ()
-  -- it "Sample" $ do
-  --     day10 testInput `shouldBe` ["", ""]
+  it "Sample" $ do
+      day10 testInput `shouldBe` ["13140", sp2]
       
-  -- it "Actual" $ do
-  --   withFile "inputs/day10.txt" ReadMode (\h -> do
-  --     actualInput <- hGetContents h
-  --     day10 actualInput `shouldBe` ["",""])
+  it "Actual" $ do
+    withFile "inputs/day10.txt" ReadMode (\h -> do
+      actualInput <- hGetContents h
+      day10 actualInput `shouldBe` ["13060", ap2])
