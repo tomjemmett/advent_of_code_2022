884d21b70d388db944bc1e9625020f3909cb3662
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-10 23:49:55 +0000

    refactors day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,36 +1,38 @@
-{-# LANGUAGE ViewPatterns #-}
 module Day10 (
   day10
 ) where
 
-
 import Common
+import Control.Monad.State (MonadState(put, get), runState, State)
 import Data.List.Split (chunksOf)
 
 day10 :: AOCSolution
-day10 input = [part1, part2] <*> pure (parseInput input)
-
-part1 :: [Int] -> String
-part1 = show . sum . f 20 . drop 19
+day10 input = [show p1, unlines $ init $ chunksOf 40 ('#':p2)]
   where
-    f _ [] = []
-    f i (x:xs) = (x * i):g i xs
-    g i = f (i + 40) . drop 39
+    (p2, (_, _, p1)) = runState (traverse go $ words input) (0, 1, 0)
 
-part2 :: [Int] -> String
-part2 = unlines . chunksOf 40 . go . zip [0..]
+-- go takes the next instruction word and modifies a state containing
+-- * cycle number
+-- * x register value
+-- * the current sum for part 1
+-- it returns whether the pixel is lit or not in that cycle
+go :: String -> State (Int, Int, Int) Char
+go = \case
+  "noop"      -> f 0
+  "addx"      -> f 0 
+  (read -> a) -> f a
   where
-    go [] = []
-    go ((x, y):xs) = p:go xs
-      where
-        p = if abs ((x `mod` 40) - y) <= 1
-          then '#'
-          else '.'
-
-parseInput :: String -> [Int]
-parseInput = (1:) . parseLine 1 . lines
-  where
-    parseLine :: Int -> [String] -> [Int]
-    parseLine _ [] = []
-    parseLine regx ("noop":xs) = regx:parseLine regx xs
-    parseLine regx (((regx+) . read . drop 5 -> n):xs) = regx:n:parseLine n xs
+    f a = do
+      -- get the current state:
+      -- * increment the counter
+      -- * update the x register by adding in a
+      -- * get the current value for part 1
+      (succ -> c, (+a) -> x, p) <- get
+      let
+        cm = c `mod` 40
+        -- update the current value for part 2
+        p' = p + if cm == 19 then succ c * x else 0
+      -- update the state
+      put (c, x, p')
+      -- return the pixel for this cycle
+      pure $ if abs (cm - x) <= 1 then '#' else '.'
