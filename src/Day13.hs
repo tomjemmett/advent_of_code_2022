67f46a84e323e3c867e08252561040e43169ae08
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-13 22:26:02 +0000

    adds day 13 solution

--- src/Day13.hs
+++ src/Day13.hs
@@ -3,6 +3,61 @@ module Day13 (
 ) where
 
 import Common
+import Data.List.Split (splitOn)
+
+import Data.Maybe (fromJust)
+import Data.List (sort, elemIndex)
+
+import qualified Text.Parsec as P
+import Text.Parsec ((<|>))
+import Text.Parsec.String (Parser)
+
+data Item = Val Int | List [Item] deriving (Show, Eq)
+
+instance Ord Item where
+  compare (Val l) (Val r) = compare l r
+  compare (List ls) (List rs) = cmp ls rs
+    where
+      cmp :: [Item] -> [Item] -> Ordering
+      cmp [] [] = EQ
+      cmp [] _  = LT
+      cmp _  [] = GT
+      cmp (l:ls) (r:rs) = let c = compare l r in
+        if c == EQ
+          then cmp ls rs
+          else c
+  compare v@(Val _) l@(List _) = compare (List [v]) l
+  compare l@(List _) v@(Val _) = compare l (List [v])
 
 day13 :: AOCSolution
-day13 input = undefined
+day13 input = show <$> [part1 i, part2 i]
+  where
+    i = parseInput input
+
+part1 :: [[Item]] -> Int
+part1 = f 1 . map (uncurry compare . tuplify2)
+  where
+    f :: Int -> [Ordering] -> Int
+    f n x
+      | null x    = 0
+      | x1 == LT  = n + next
+      | otherwise = 0 + next
+      where
+        (x1:xs) = x
+        next = f (succ n) xs
+
+part2 :: [[Item]] -> Int
+part2 i = product $ succ . fromJust . (`elemIndex` i') <$> d
+  where
+    d = [List [List [Val x]] | x <- [2, 6]]
+    i' = sort $ d ++ concat i
+
+parseInput :: String -> [[Item]]
+parseInput = map2 (parse' parseItem id) . map (splitOn "\n") . splitOn "\n\n"
+
+parseItem :: Parser Item
+parseItem =
+  P.choice [
+    Val <$> number,
+    List <$> (P.char '[' *> parseItem `P.sepBy` P.char ',' <* P.char ']')
+  ]
--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -2,15 +2,36 @@ module Day13Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "[1,1,3,1,1]\n\
+            \[1,1,5,1,1]\n\
+            \\n\
+            \[[1],[2,3,4]]\n\
+            \[[1],4]\n\
+            \\n\
+            \[9]\n\
+            \[[8,7,6]]\n\
+            \\n\
+            \[[4,4],4,4]\n\
+            \[[4,4],4,4,4]\n\
+            \\n\
+            \[7,7,7,7]\n\
+            \[7,7,7]\n\
+            \\n\
+            \[]\n\
+            \[3]\n\
+            \\n\
+            \[[[]]]\n\
+            \[[]]\n\
+            \\n\
+            \[1,[2,[3,[4,[5,6,7]]]],8,9]\n\
+            \[1,[2,[3,[4,[5,6,0]]]],8,9]"
 
 spec :: Spec
 spec = describe "Day 13" $ do
-  return ()
-  -- it "Sample" $ do
-  --     day13 testInput `shouldBe` ["", ""]
+  it "Sample" $ do
+      day13 testInput `shouldBe` ["13", "140"]
       
-  -- it "Actual" $ do
-  --   withFile "inputs/day13.txt" ReadMode (\h -> do
-  --     actualInput <- hGetContents h
-  --     day13 actualInput `shouldBe` ["",""])
+  it "Actual" $ do
+    withFile "inputs/day13.txt" ReadMode (\h -> do
+      actualInput <- hGetContents h
+      day13 actualInput `shouldBe` ["6369","25800"])
