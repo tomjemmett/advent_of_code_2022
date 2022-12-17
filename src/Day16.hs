2244ff09ae24747b043d1761bd43b0d67dfd62bd
Tom Jemmett <thomas.jemmett@nhs.net>
2022-12-17 21:03:29 +0000

    adds day 16 solution

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,8 +1,77 @@
-module Day16 (
-  day16
-) where
+module Day16
+  ( day16,
+  )
+where
 
 import Common
+import Data.Char (isUpper)
+import Data.Function ((&))
+import Data.Graph.Inductive hiding ((&))
+import Data.List (foldl', maximumBy)
+import Data.List.Split (splitOn)
+import Data.Map.Strict qualified as M
+import Data.Ord (comparing)
+import Data.Set qualified as S
+import Data.Tree qualified as T
 
 day16 :: AOCSolution
-day16 input = undefined
+day16 input = show <$> [p1 i, p2 i]
+  where
+    i = parseInput input
+
+each :: [a] -> (a -> b) -> [b]
+each = flip map
+
+parseInput :: String -> (Node, M.Map Node Int, M.Map Node (M.Map Node Int))
+parseInput inp = (start, valves, dists)
+  where
+    xs = M.fromList . map (\(i, (v, r, vs)) -> (v, (i, r, vs))) . zip [0 ..] . map pLine $ lines inp
+    pLine
+      ( splitOn ";" ->
+          [ words -> [_, v, _, _, read . drop 5 -> r],
+            splitOn ", " . dropWhile (not . isUpper) -> vs
+            ]
+        ) = (v, r, vs)
+
+    gr = undir $ foldl' f empty xs :: Gr Int ()
+    f acc (i, r, vs) =
+      acc
+        & insNode (i, r)
+        & insEdges [(i, j, ()) | w <- vs, let (j, _, _) = xs M.! w]
+
+    (start, _, _) = xs M.! "AA"
+    valves = M.fromList . map (\(u, r, _) -> (u, r)) . filter (\(_, r, _) -> r > 0) $ M.elems xs
+    dists = M.fromList . each (start : M.keys valves) $ \i ->
+      ( i,
+        M.fromList . each (bft i gr) $ \p ->
+          (head p, length p - 1)
+      )
+
+stateTree :: Int -> M.Map Node (M.Map Node Int) -> Node -> M.Map Node Int -> T.Tree (Node, Int)
+stateTree maxT dists start valves = T.unfoldTree buildNode (1, start, M.empty)
+  where
+    nextStates (t, v, zs) =
+      if
+          | t >= maxT ->
+            []
+          | v /= start && v `M.notMember` zs ->
+            [(t + 1, v, M.insert v ((maxT - t) * valves M.! v) zs)]
+          | otherwise ->
+            [(t + dists M.! v M.! w, w, zs) | w <- M.keys valves, w `M.notMember` zs]
+    buildNode (t, v, zs) =
+      let succs = nextStates (t, v, zs)
+       in ((v, if null succs then foldl' (+) 0 zs else 0), succs)
+
+leaves :: T.Tree (a, b) -> [([a], b)]
+leaves (T.Node (n, v) []) = [([n], v)]
+leaves (T.Node (n, _) xs) = map (\(ns, v) -> (n : ns, v)) $ concatMap leaves xs
+
+p1 (s, vs, dists) = maximum . map snd . leaves $ tr
+  where
+    tr = stateTree 30 dists s vs
+
+p2 (s, vs1, dists) = f1 + f2
+  where
+    (p, f1) = maximumBy (comparing snd) . leaves $ stateTree 26 dists s vs1
+    vs2 = vs1 `M.withoutKeys` S.fromList p
+    (_, f2) = maximumBy (comparing snd) . leaves $ stateTree 26 dists s vs2
--- test/Day16Spec.hs
+++ test/Day16Spec.hs
@@ -1,16 +1,28 @@
-module Day16Spec ( spec ) where
+module Day16Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\n\
+  \Valve BB has flow rate=13; tunnels lead to valves CC, AA\n\
+  \Valve CC has flow rate=2; tunnels lead to valves DD, BB\n\
+  \Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE\n\
+  \Valve EE has flow rate=3; tunnels lead to valves FF, DD\n\
+  \Valve FF has flow rate=0; tunnels lead to valves EE, GG\n\
+  \Valve GG has flow rate=0; tunnels lead to valves FF, HH\n\
+  \Valve HH has flow rate=22; tunnel leads to valve GG\n\
+  \Valve II has flow rate=0; tunnels lead to valves AA, JJ\n\
+  \Valve JJ has flow rate=21; tunnel leads to valve II"
 
 spec :: Spec
 spec = describe "Day 16" $ do
-  return ()
-  -- it "Sample" $ do
-  --     day16 testInput `shouldBe` ["", ""]
-      
-  -- it "Actual" $ do
-  --   withFile "inputs/day16.txt" ReadMode (\h -> do
-  --     actualInput <- hGetContents h
-  --     day16 actualInput `shouldBe` ["",""])
+  it "Sample" $ do
+    day16 testInput `shouldBe` ["1651", "1327"] -- "1707"]
+  it "Actual" $ do
+    withFile
+      "inputs/day16.txt"
+      ReadMode
+      ( \h -> do
+          actualInput <- hGetContents h
+          day16 actualInput `shouldBe` ["1701", "2455"]
+      )
