ad4fb589c7369e47486aab626f2ca31eaa1e77fd
Tom Jemmett <tom.jemmett@gmail.com>
2022-12-26 23:06:19 +0000

    adds day 25 solution

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,8 +1,26 @@
-module Day25 (
-  day25
-) where
+module Day25
+  ( day25,
+  )
+where
 
 import Common
 
 day25 :: AOCSolution
-day25 input = undefined
+day25 = (: []) . decimalToSnafu . sum . map (snafuToDecimal . reverse) . lines
+
+snafuToDecimal :: String -> Int
+snafuToDecimal [] = 0
+snafuToDecimal (x : xs) =
+  snafuToDecimal xs * 5 + case x of
+    '2' -> 2
+    '1' -> 1
+    '0' -> 0
+    '-' -> -1
+    '=' -> -2
+    _ -> undefined
+
+decimalToSnafu :: Int -> String
+decimalToSnafu 0 = ""
+decimalToSnafu d = decimalToSnafu a ++ ["=-012" !! b]
+  where
+    (a, b) = (d + 2) `divMod` 5
--- test/Day25Spec.hs
+++ test/Day25Spec.hs
@@ -1,16 +1,32 @@
-module Day25Spec ( spec ) where
+module Day25Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "1=-0-2\n\
+  \12111\n\
+  \2=0=\n\
+  \21\n\
+  \2=01\n\
+  \111\n\
+  \20012\n\
+  \112\n\
+  \1=-1=\n\
+  \1-12\n\
+  \12\n\
+  \1=\n\
+  \122"
 
 spec :: Spec
 spec = describe "Day 25" $ do
-  return ()
-  -- it "Sample" $ do
-  --     day25 testInput `shouldBe` ["", ""]
-      
-  -- it "Actual" $ do
-  --   withFile "inputs/day25.txt" ReadMode (\h -> do
-  --     actualInput <- hGetContents h
-  --     day25 actualInput `shouldBe` ["",""])
+  it "Sample" $ do
+    day25 testInput `shouldBe` ["2=-1=0"]
+
+  it "Actual" $ do
+    withFile
+      "inputs/day25.txt"
+      ReadMode
+      ( \h -> do
+          actualInput <- hGetContents h
+          day25 actualInput `shouldBe` ["2-==10--=-0101==1201"]
+      )
